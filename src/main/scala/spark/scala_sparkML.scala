30c7b6872c3272b2b485c94b86970e7fd51aeac7
ManeeshaR <rmaneesha29@gmail.com>
2025-03-11 16:12:08 +0000

    first commit

--- src/main/scala/spark/scala_sparkML.scala
+++ src/main/scala/spark/scala_sparkML.scala
@@ -1,8 +1,4 @@
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.ml.feature.{VectorAssembler, StringIndexer}
-import org.apache.spark.ml.regression.RandomForestRegressor
-import org.apache.spark.ml.evaluation.RegressionEvaluator
-import org.apache.spark.ml.Pipeline
 
 object TFLUndergroundPredictor {
   def main(args: Array[String]): Unit = {
